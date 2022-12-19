cab0a847f39745f48383a15fc55ae8b4461d45dc
VictorLascares <vmlascares@gmail.com>
2022-12-18 20:23:29 -0600

    Agregando hojas de estilos con Links

--- app/root.jsx
+++ app/root.jsx
@@ -1,11 +1,21 @@
-import { Meta } from "@remix-run/react";
+import { Meta, Links } from "@remix-run/react";
+import styles from "./styles/index.css";
 
 export function meta() {
   return {
     charset: "utf-8",
-    title: "GuitarLA - Remix",
     viewport: "width=device-width,initial-scale=1",
+    title: "GuitarLA - Remix",
   };
+}
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
 }
 
 export default function App() {
@@ -21,6 +31,7 @@ function Document({ children }) {
     <html lang="es">
       <head>
         <Meta />
+        <Links />
       </head>
 
       <body>{children}</body>
